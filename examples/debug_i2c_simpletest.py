ea57bdd72c1327e59fec8cd1390831d5f1a41c6a
Kattni Rembor <kattni@adafruit.com>
2019-11-05 21:14:48 -0500

    Linting

--- examples/debug_i2c_simpletest.py
+++ examples/debug_i2c_simpletest.py
@@ -1,9 +1,9 @@
 """This example uses the LIS3DH accelerometer. Debug_I2C can be used with any I2C device."""
-import adafruit_lis3dh
-from adafruit_debug_i2c import DebugI2C
 import busio
 import board
 import digitalio
+from adafruit_debug_i2c import DebugI2C
+import adafruit_lis3dh
 
 i2c = DebugI2C(busio.I2C(board.SCL, board.SDA))
 int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
